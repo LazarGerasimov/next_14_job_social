0004770b29e6f20afe941821980d927271f25063
Laz <lazgerasimov@gmail.com>
2024-05-19 13:02:46 +0300

    setup homepage scaffold

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Job Social</h1>
-      <Button>Click me</Button>
-    </main>
+    <div className="grid">
+      <section>
+        {/* User information */}
+      </section>
+
+      <section>
+        {/* Post Form (Create Post) */}
+        {/* PostFeed */}
+      </section>
+
+      <section>
+        {/* Widget / Sidebar */}
+      </section>
+    </div>
   );
 }
