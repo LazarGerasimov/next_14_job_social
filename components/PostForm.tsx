c3cb0a81238c963461158a0efb71f64424e972ad
Laz <lazgerasimov@gmail.com>
2024-05-21 19:33:42 +0300

    add post form input & styles

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -3,14 +3,14 @@
 import { useUser } from "@clerk/nextjs"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
-const PostForm = async () => {
+const PostForm = () => {
 
   const { user } = useUser();
 
   return (
     <div>
       <form action="">
-        <div>
+        <div className="flex items-center space-x-2">
           <Avatar>
             <AvatarImage src={user?.imageUrl || "https://github.com/shadcn.png"} />
             <AvatarFallback>
@@ -18,6 +18,13 @@ const PostForm = async () => {
               {user?.lastName?.charAt(0)}
             </AvatarFallback>
           </Avatar>
+
+          <input
+            type="text"
+            name="postInput"
+            placeholder="Write a post.."
+            className="flex-1 outline-none rounded-full py-3 px-4 border"
+          />
         </div>
       </form>
     </div>
