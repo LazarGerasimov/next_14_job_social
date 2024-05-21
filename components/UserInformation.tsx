84ef731b5fc1abf6fcfb142bcc2c689868a75859
Laz <lazgerasimov@gmail.com>
2024-05-21 19:10:42 +0300

    add posts and comments sections

--- components/UserInformation.tsx
+++ components/UserInformation.tsx
@@ -45,6 +45,19 @@ const UserInformation = async () => {
       </SignedOut>
 
       <hr className="w-full border-gray-200 my-5" />
+
+      {/* Posts */}
+      <div className="flex justify-between w-full px-4 text-sm">
+        <p className="font-semibold text-gray-400">Posts</p>
+        <p className="text-blue-400">5</p>
+      </div>
+
+      {/* Comments */}
+      <div className="flex justify-between w-full px-4 text-sm">
+        <p className="font-semibold text-gray-400">Comments</p>
+        <p className="text-blue-400">4</p>
+      </div>
+
     </div>
   )
 }
