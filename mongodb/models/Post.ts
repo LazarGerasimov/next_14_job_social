7b7f5ad570d87b75a7ab6030ba81b3f2c4f1b056
Laz <lazgerasimov@gmail.com>
2024-05-21 21:15:52 +0300

    add post methods interface

--- mongodb/models/Post.ts
+++ mongodb/models/Post.ts
@@ -1,6 +1,6 @@
 import { IUser } from "@/types";
 import mongoose, { Schema, Document, models, Model } from "mongoose";
-import { IComment } from "./Comment";
+import { IComment, ICommentBase } from "./Comment";
 
 export interface IPostBase {
   user: IUser;
@@ -8,4 +8,17 @@ export interface IPostBase {
   imageUrl?: string;
   comments?: IComment[];
   likes?: string;
+}
+
+export interface IPost extends IPostBase, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface IPostMethods {
+  likePost(userId: string): Promise<void>;
+  unlikePost(userId: string): Promise<void>;
+  commentOnPost(comment: ICommentBase): Promise<void>;
+  getAllComments(): Promise<IComment[]>;
+  removePost(): Promise<void>;
 }
