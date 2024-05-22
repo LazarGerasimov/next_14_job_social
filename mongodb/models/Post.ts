4fb82e6a234ad0324de4bdc2730395d523829ffe
Laz <lazgerasimov@gmail.com>
2024-05-22 20:38:45 +0300

    define post model schema

--- mongodb/models/Post.ts
+++ mongodb/models/Post.ts
@@ -22,3 +22,30 @@ interface IPostMethods {
   getAllComments(): Promise<IComment[]>;
   removePost(): Promise<void>;
 }
+
+interface IPostStatics {
+  getAllPosts(): Promise<IPostDocument[]>;
+}
+
+export interface IPostDocument extends IPost, IPostMethods { }; // singular instance of a post
+
+interface IPostModel extends IPostStatics, Model<IPostDocument> { }; // all posts
+
+const PostSchema = new Schema<IPostDocument>(
+  {
+    user: {
+      userId: { type: String, required: true },
+      userImage: { type: String, required: true },
+      firstName: { type: String, required: true },
+      lastName: { type: String }
+    },
+    text: { type: String, required: true },
+    imageUrl: { type: String },
+    comments: { type: [Schema.Types.ObjectId], ref: "Comment", default: [] },
+    likes: { type: [String] }
+  },
+  {
+    timestamps: true,
+  }
+);
+
