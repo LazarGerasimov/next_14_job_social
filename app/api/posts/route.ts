52caf0befba44b8293e14c95dbe8a0ffadad6fe0
Laz <lazgerasimov@gmail.com>
2024-06-02 19:15:00 +0300

    Create get-all-posts request

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,4 +1,7 @@
+import connectDB from "@/mongodb/db";
+import { Post } from "@/mongodb/models/Post";
 import { IUser } from "@/types";
+import { NextResponse } from "next/server";
 
 
 export interface AddPostRequestBody {
@@ -12,5 +15,18 @@ export async function POST(request: Request) {
 }
 
 export async function GET(request: Request) {
+  try {
+    
+    await connectDB(); // connect to database 
 
+    const posts = await Post.getAllPosts();
+
+    return NextResponse.json({ posts });
+
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An error occurred while fetching posts" },
+      { status: 500 }
+    );
+  }
 }
