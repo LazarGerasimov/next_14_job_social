1329c0155b027f37c6b24a4049011629f41d2cf6
Laz <lazgerasimov@gmail.com>
2024-06-03 19:40:02 +0300

    create create-post api request

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,6 +1,7 @@
 import connectDB from "@/mongodb/db";
-import { Post } from "@/mongodb/models/Post";
+import { IPostBase, Post } from "@/mongodb/models/Post";
 import { IUser } from "@/types";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
@@ -11,12 +12,36 @@ export interface AddPostRequestBody {
 }
 
 export async function POST(request: Request) {
+  auth().protect(); // Protect the route with Clerk authentication;
+
+
+
+  try {
+    const { user, text, imageUrl }: AddPostRequestBody = await request.json();
+
+    const postData: IPostBase = {
+      user,
+      text,
+      ...(imageUrl && { imageUrl }), // if image URL is available, spread 
+    };
+
+    const post = await Post.create(postData);
+
+    return NextResponse.json({ message: "Post created successfully", post });
+
+  } catch (error) {
+    return NextResponse.json(
+      { error: `An error occured while creating the post ${error}` },
+      { status: 500 }
+    )
+  }
+
 
 }
 
 export async function GET(request: Request) {
   try {
-    
+
     await connectDB(); // connect to database 
 
     const posts = await Post.getAllPosts();
