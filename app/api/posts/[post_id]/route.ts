8671e296d5801ac336129595d1e0f24b9f81f8e2
Laz <lazgerasimov@gmail.com>
2024-06-04 22:48:19 +0300

    create delete post api request

--- app/api/posts/[post_id]/route.ts
+++ app/api/posts/[post_id]/route.ts
@@ -1,5 +1,6 @@
 import connectDB from "@/mongodb/db";
 import { Post } from "@/mongodb/models/Post";
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(
@@ -19,6 +20,43 @@ export async function GET(
   } catch (error) {
     return NextResponse.json(
       { error: "An error occurred while fetching this post" }
+    )
+  }
+};
+
+// export interface DeletePostRequestBody {
+//   userId: string;
+// }
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { post_id: string } }
+) {
+  auth().protect(); // protected route 
+
+  const user = await currentUser();
+
+  await connectDB();
+
+  // const { userId }: DeletePostRequestBody = await request.json();  - check later 
+
+  try {
+    const post = await Post.findById(params.post_id);
+
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
+
+    if (post.user.userId !== user?.id) {
+      throw new Error("Post does not belong to this user");
+    }
+
+    await post.removePost();
+
+    return NextResponse.json({ message: "Post delete successfully" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An error occurred while deleting the post" }
     )
   }
 }
