f1194821403e24c6cd963265ee4c8c6a115462bc
Laz <lazgerasimov@gmail.com>
2024-06-04 23:04:46 +0300

    create get all likes api endpoint

---
+++ app/api/posts/[post_id]/like/route.ts
@@ -0,0 +1,28 @@
+import connectDB from "@/mongodb/db";
+import { Post } from "@/mongodb/models/Post";
+
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { post_id: string } }
+) {
+  await connectDB();
+
+  try {
+    const post = await Post.findById(params.post_id);
+
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
+
+    const likes = post.likes;
+    return NextResponse.json(likes);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An error occurred while fetching likes" },
+      { status: 500 }
+    );
+  }
+}
+
