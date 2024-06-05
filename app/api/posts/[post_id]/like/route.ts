39833552b7c009f304a65e926126ca812e011626
Laz <lazgerasimov@gmail.com>
2024-06-05 11:23:22 +0300

    create like post api endpoint

--- app/api/posts/[post_id]/like/route.ts
+++ app/api/posts/[post_id]/like/route.ts
@@ -26,3 +26,31 @@ export async function GET(
   }
 }
 
+export interface LikePostRequestBody {
+  userId: string;
+}
+
+export async function POST(
+  request: Request,
+  { params }: { params: { post_id: string } }
+) {
+  await connectDB();
+
+  const { userId }: LikePostRequestBody = await request.json();
+
+  try {
+    const post = await Post.findById(params.post_id);
+
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
+
+    await post.likePost(userId);
+    return NextResponse.json({ message: "Post liked successfully" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An error occurred while liking the post" },
+      { status: 500 }
+    );
+  }
+}
